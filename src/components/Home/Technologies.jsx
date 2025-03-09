dc67ea1438a2150ea6d2e00f4bda5f869983d288
Jakub <jakub.kasprzak009@gmail.com>
2025-03-09 01:51:14 +0100

    web technologies

--- src/components/Home/Technologies.jsx
+++ src/components/Home/Technologies.jsx
@@ -1,41 +1,48 @@
-import React from 'react'
+import React from 'react';
 
-// Imgs
 import react from "../../assets/icons/react.png";
 import tailwind from "../../assets/icons/tailwind.png";
 import js from "../../assets/icons/js.png";
 import css from "../../assets/icons/css.png";
 import html from "../../assets/icons/html.png";
-import ts from "../../assets/icons/ts.png"
+import ts from "../../assets/icons/ts.png";
 import pc from "../../assets/computer.png";
 
 const technologies = [
-  { src: react, alt: "React" },
-  { src: tailwind, alt: "Tailwind" },
-  { src: js, alt: "JS" },
-  { src: html, alt: "HTML" },
-  { src: ts, alt: "TS" },
-  { src: css, alt: "CSS" }
+  { src: react, alt: "React", link: "https://reactjs.org/" },
+  { src: tailwind, alt: "Tailwind", link: "https://tailwindcss.com/" },
+  { src: js, alt: "JavaScript", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { src: html, alt: "HTML", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+  { src: ts, alt: "TypeScript", link: "https://www.typescriptlang.org/" },
+  { src: css, alt: "CSS", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" }
 ];
-
-//TODO: Fix the tailwindcss icon
 
 export default function Technologies() {
   return (
-    <section className="bg-gradient-to-b bg-[#1c1c2b] text-white min-h-[750px] h-auto flex flex-col items-center justify-center text-center px-8 py-16 gap-y-8">
-      <h1 className="mb-12 text-4xl font-bold">Technologie Webowe</h1>
-
-      <ul className="grid w-full grid-cols-2 gap-8 sm:grid-cols-3 lg:grid-cols-6">
-        {technologies.map(({ src, alt }) => (
-          <li key={alt} className="flex flex-col items-center group">
-            <div className="flex items-center justify-center w-24 h-24 transition-transform duration-300 border-2 border-black rounded-full bg-[#29293d] group-hover:scale-110">
-              <img src={src} alt={alt} className="w-14 h-14" />
-            </div>
+    <section className="bg-gradient-to-b bg-[#1c1c2b] w-full text-white h-auto flex flex-col justify-between items-center gap-y-20">
+      <h1 className="my-16 text-4xl font-bold text-center lg:text-5xl xl:text-6xl">Technologie Webowe</h1>
+      <ul className="grid w-auto grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-6">
+        {technologies.map(({ src, alt, link }) => (
+          <li key={alt} className="flex flex-col group">
+            <a
+              href={link}
+              title={alt}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-full h-full"
+            >
+              <div className="flex items-center justify-center w-32 h-32 transition-transform duration-300 border-2 border-black rounded-full bg-[#29293d] group-hover:scale-105 group-hover:bg-[#383850]">
+                <img src={src} alt={alt} className="w-16 h-16" />
+              </div>
+            </a>
           </li>
         ))}
       </ul>
-
-      <img src={pc} alt="laptop" className="mt-12 w-80" />
+      
+      <div className="relative flex items-center justify-center w-full mt-16">
+        <div className="absolute bottom-0 left-0 w-[120%] h-[150px] bg-[#b0b0b0] rounded-t-[100%] -ml-[10%]"></div>
+        <img src={pc} alt="laptop" className="w-[30rem] z-10" />
+      </div>
     </section>
   );
 }
